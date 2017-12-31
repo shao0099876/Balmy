de16aa5ac66b6d8e6acbe444c733c137c61cfb3a
若忱 邵 <shao0099876@outlook.com>
2017-12-31 14:01:20 +0800

    加入SaveAll按钮

--- src/ui/toolbar/SaveAllFileButton.java
+++ src/ui/toolbar/SaveAllFileButton.java
@@ -6,16 +6,17 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+import ui.ActionPerformFunction;
 import ui.UserInterface;
 
 public class SaveAllFileButton extends JButton{
 	public SaveAllFileButton(UserInterface face) {
 		super(new ImageIcon(".\\Icon\\SaveAllFile.png"));
-		setToolTipText("Save All File");
+		setToolTipText("Save All");
 		addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				ActionPerformFunction.saveAllAction();
 			}
 		});
 	}
